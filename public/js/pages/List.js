9cdd974f27b5077eb2b6682bb5caa1740b75359a
sunckle <sprunkibfdipingoofy@gmail.com>
2026-08-01 09:52:19 -0700

    Update List.js

--- public/js/pages/List.js
+++ public/js/pages/List.js
@@ -26,7 +26,7 @@ export default {
 
 <div
     class="featured-level"
-    @click="selected = featuredIndex"
+    @click="selected = list.findIndex(([l]) => l?.id === list[featuredIndex][0].id)"
 >
 
     <div class="featured-title">
